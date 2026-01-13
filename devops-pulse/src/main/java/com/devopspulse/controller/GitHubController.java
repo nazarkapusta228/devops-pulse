68fb700e8105cccc67274fce677a7ac48bdc3a8f
Nazar Kapustynskyi <258522@edu.p.lodz.pl>
2026-01-13 22:04:09 +0100

    Complete basic REST API structure

--- devops-pulse/src/main/java/com/devopspulse/controller/GitHubController.java
+++ devops-pulse/src/main/java/com/devopspulse/controller/GitHubController.java
@@ -1,4 +1,27 @@
 package com.devopspulse.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+
 public class GitHubController {
+
+    @GetMapping("/github/{username}")
+    public Map<String, Object> getGithubInfo(@PathVariable String username){
+        Map<String, Object> githubInfo = new HashMap<>();
+        githubInfo.put("username", username);
+        githubInfo.put("publicRepos", 1);
+        githubInfo.put("followers", 0);
+        githubInfo.put("following", 0);
+        githubInfo.put("accountCreatedAt", "2023-01-01");
+        return githubInfo;
+    }
 }
--- devops-pulse/src/main/java/com/devopspulse/controller/SystemController.java
+++ devops-pulse/src/main/java/com/devopspulse/controller/SystemController.java
@@ -1,4 +1,37 @@
 package com.devopspulse.controller;
 
+import io.micrometer.observation.annotation.ObservationKeyValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+
+
 public class SystemController {
+
+    @GetMapping("/system")
+    public Map<String, Object> getSystemInfo(){
+        Map<String, Object> systemInfo = new HashMap<>();
+        systemInfo.put("cpuUsage", 15.5);
+        systemInfo.put("memoryUsage", 62.5);
+        systemInfo.put("diskUsage", 42.4);
+        systemInfo.put("status", "healthy");
+        systemInfo.put("timestamp", 1744651234);
+        return systemInfo;
+    }
+
+
+    @GetMapping("/health")
+    public Map<String, Object> customHealthCheck(){
+        Map<String, Object> healthInfo = new HashMap<>();
+        healthInfo.put("status", "UP");
+        healthInfo.put("service", "DevOps Pulse API");
+        healthInfo.put("version", "1.0.0");
+        return healthInfo;
+    }
 }
